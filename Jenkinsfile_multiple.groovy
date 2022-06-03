f66adfe8218f6f2b79abaa3d8e1f2ebecdb87e16
Somasekhar Kuruva <somasekhar.kuruva@trianz.com>
2022-06-03 07:42:56 +0530

    added for all6

--- Jenkinsfile_multiple.groovy
+++ Jenkinsfile_multiple.groovy
@@ -8,7 +8,7 @@ pipeline{
                 sh """
                 echo $BUILD_NUMBER $BRANCH $SERVERIPS
                 IFS="," read -r -a MYSERVERIPS <<< $SERVERIPS
-                echo $MYSERVERIPS[@]
+                echo ${MYSERVERIPS[@]}
 
                 """
                 }
